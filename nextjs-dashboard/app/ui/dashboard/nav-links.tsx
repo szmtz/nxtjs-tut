8d54feb4771f972bd6a0dcb24009233e7ef96c3e
saulzmartinez <saulzmartinez@gmail.com>
2025-03-01 22:03:46 +0000

    lesson6-1

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -7,6 +7,7 @@ import {
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
 import Link from 'next/link'; 
+import { usePathname } from 'next/navigation'; 
 import clsx from 'clsx'; 
 
 
